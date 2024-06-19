babf2491f58134c5ac927448ed11f363e7851285
adamshamree <101863902+adamshamree@users.noreply.github.com>
2024-06-19 15:18:43 +0800

    Update ReviewController.php

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -10,6 +10,6 @@ class ReviewController extends Controller
     public function show($id)
     {
         $Review = Review::findOrFail($id);
-        return view('review.show', compact('review'));
+        return view('review', compact('review'));
     }
 }
