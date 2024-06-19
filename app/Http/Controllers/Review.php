0c2f28f12c80a90607b4f950ed580eb72c1b4ca0
adamshamree <101863902+adamshamree@users.noreply.github.com>
2024-06-19 15:27:17 +0800

    Update Review.php

--- app/Http/Controllers/Review.php
+++ app/Http/Controllers/Review.php
@@ -1,15 +1,5 @@
 <?php
 
-$DATABASE_HOST = 'localhost';
-$DATABASE_USER = 'root';
-$DATABASE_PASS = '';
-$DATABASE_NAME = 'reviews';
-try {
-    $pdo = new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
-} catch (PDOException $exception) {
-    // If there is an error with the connection, stop the script and display the error.
-    exit('Failed to connect to database!');
-}
 function time_elapsed_string($datetime, $full = false) {
     $now = new DateTime;
     $ago = new DateTime($datetime);
